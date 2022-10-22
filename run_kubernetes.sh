096f8d5013d75d6108abd35e51dee3290165f4e2
Kapil24nagar <Kapil24nagar@gmail.com>
2022-10-22 16:47:22 +0530

    PRoject 4

---
+++ run_kubernetes.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=kapil24nagar/mlpred
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+r=$(kubectl get pod mlpred 2> /dev/null;echo $?)
+
+if [ "_$r" == "_1" ] ; then
+    kubectl run mlpred \
+    --image=$dockerpath \
+    --image-pull-policy="Always" \
+    --overrides='{"apiVersion": "v1", "spec":{"imagePullSecrets": [{"name": "regcred"}]}}'
+fi
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+# kubectl port-forward mlpred 8080:5000
